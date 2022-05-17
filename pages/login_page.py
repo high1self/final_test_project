d40371c939450a2061a005b84f9cea51bc2db5cb
Kirill Kozhevnikov <k.kozhevnikov@likebz.ru>
2022-05-17 17:40:54 +0300

    Final 4.2 section

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,13 +10,13 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        login = "loginn"
+        login = "login"
         assert login in self.browser.current_url, f"the url is not contains {login}"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(LoginPageLocators.FORM_LOGIN), "There is no login form"
+        assert self.is_element_present(*LoginPageLocators.FORM_LOGIN), "There is no login form"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(LoginPageLocators.FORM_REG), "There is no registration form"
+        assert self.is_element_present(*LoginPageLocators.FORM_REG), "There is no registration form"
--- pages/main_page.py
+++ pages/main_page.py
@@ -5,8 +5,8 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
 
     def should_be_login_link(self):
