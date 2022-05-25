370f45fefd2657dd3af1ffc0385a55f9e969c00d
Kirill Kozhevnikov <k.kozhevnikov@likebz.ru>
2022-05-25 18:55:46 +0300

    Плюсы наследования: пример

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,8 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
+
 
 class BasePage():
     # инициализация
@@ -40,3 +42,10 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 class LoginPageLocators():
     FORM_LOGIN = (By.CSS_SELECTOR, "#login_form")
     FORM_REG = (By.CSS_SELECTOR, "#register_form")
@@ -24,3 +21,7 @@ class ProductPageLocators():
     BUSKET_PRICE = (By.CSS_SELECTOR, ".alert.alert-safe.alert-noicon.alert-info.fade.in>.alertinner>p>strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main>.price_color")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>:nth-child(1) .alertinner>strong")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
